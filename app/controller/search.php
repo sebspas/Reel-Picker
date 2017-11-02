408374c641a776d5bfc82f9d2b2c2e88d846dbd0
Boud <maxime.boudreault.0@gmail.com>
2017-11-02 13:21:41 -0400

    Redirect to home with search terms on search

--- app/controller/search.php
+++ app/controller/search.php
@@ -1,4 +1,13 @@
 <?php
+    $BD = new BD('movie');
+    $results = array();
 
-    echo $twig->render('search.twig', array('connected' => $_SESSION['login'], 'page' => $_GET['page']));
+    // check if the user post the search form
+    if (isset($_POST['search'])) {
+        $searchTerms = filter_var($_POST['search'], FILTER_SANITIZE_STRING);
+        $searchTerms = str_replace(" ", "+", $searchTerms);
+        header(sprintf('Location: index.php?page=home&search=%s', $searchTerms));
+    }
+
+    echo $twig->render('search.twig', array('connected' => $_SESSION['login'], 'page' => $_GET['page'], 'results' => $results));
 ?>
