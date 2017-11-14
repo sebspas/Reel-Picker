50bd61d935c2d0290f144eed82371c05f08ef892
sebspas <sebspas@gmail.com>
2017-11-14 12:19:42 -0500

    Update twig to 2.0 + Debug fully the register form

--- app/controller/register.php
+++ app/controller/register.php
@@ -2,9 +2,9 @@
 
     // include the model file
     require_once(Config::$path['model'].'register.php');
-    
-    // empty error by default
-    $error = '';
+
+    // user data of the form, empty by default
+    $data = array();
 
     // check if the user posts the register form
     if (isset($_POST['register'])) {
@@ -13,11 +13,10 @@
         if (sizeof($errors) == 0) {
             // If there are no errors, this means everything is ok and user should be able to sign in
             header('Location: index.php?page=login');
-        }
-        else {
-            $error = join($errors, "<BR>");
+        } else {
+            $data = $_POST;
         }
     }
 
-    echo $twig->render('register.twig', array('connected' => 'False', 'error' => $error));
+    echo $twig->render('register.twig', array('connected' => 'False', 'errors' => $errors, 'data' => $_POST));
 ?>
--- app/model/register.php
+++ app/model/register.php
@@ -2,14 +2,26 @@
     function TryRegister($email, $pseudo, $password, $passwordConfirm) {
         $BD = new BD('users');
         $errors = array();
-        if ($BD->isInDb("pseudo", $pseudo)) {
-            $errors.push(sprintf("The username '%s' is already taken.", $pseudo));
+
+        if (empty($pseudo)) {
+            $errors[] = ("The username must not be empty.");
         }
-        if ($BD->isInDb("email", $email)) {
-            $errors.push(sprintf("The email '%s' has already been used.", $email));
+        else if ($BD->isInDb("pseudo", $pseudo)) {
+            $errors[] = (sprintf("The username '%s' is already taken.", $pseudo));
         }
-        if ($password != $passwordConfirm) {
-            $errors.push("You must input the same password twice.");
+        
+        if (empty($email)) {
+            $errors[] = ("The mail must not be empty.");
+        }
+        else if ($BD->isInDb("email", $email)) {
+            $errors[] = (sprintf("The email '%s' has already been used.", $email));
+        }
+
+        if (empty($password)) {
+            $errors[] = ("The password must not be empty.");
+        }
+        if (empty($password) || $password != $passwordConfirm) {
+            $errors[] = ("You must input the same password twice.");
         }
 
         if(sizeof($errors) == 0){
